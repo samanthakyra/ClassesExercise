1372609118c67767a287a316d334540cf6e23a8b
samanthakyra <samanthakyramendoza@gmail.com>
2022-09-22 09:13:08 -0700

    initial commit

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassesExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car();
+            myCar.Make = "Audi";
+            myCar.Model = "A4";
+            myCar.Year = 2017;
+
+            Console.WriteLine($"My car is a {myCar.Year} {myCar.Make} {myCar.Model}.");
+
+            var yourCar = new Car();
+            {
+                yourCar.Make = "Ford";
+                yourCar.Model = "Mustang";
+                yourCar.Year = 2017;
+
+                Console.WriteLine($"Nice, I have a {yourCar.Year} {yourCar.Make} {yourCar.Model}.");
+            }
+
+        }
+    }
+}
+
